ba2f7376657ae56da8548510191c7ba32ce3d0f5
josedavid <josedavid@bestiajo.jd>
2015-11-27 17:15:56 +0100

    enable svg.cpp to generate inverse masks (old mode is new 'black', inverse is new 'white')

--- apps/svg.cpp
+++ apps/svg.cpp
@@ -10,7 +10,7 @@
 #include <iomanip>
 
 
-void writePolygonSVG(FILE * f, clp::Path &path, bool isContour, double scalingFactor, double minx, double miny) {
+void writePolygonSVG(FILE * f, clp::Path &path, bool isContour, bool insideIsBlack, double scalingFactor, double minx, double miny) {
     if (path.empty()) return;
     fprintf(f, R"(    <polygon slicer:type="%s" points=")", isContour ? "contour" : "hole");
     bool firstTime = true;
@@ -20,27 +20,27 @@ void writePolygonSVG(FILE * f, clp::Path &path, bool isContour, double scalingFa
         fprintf(f, "%s%.20g,%.20g", firstTime ? "" : " ", point->X*scalingFactor - minx, point->Y*scalingFactor - miny);
         firstTime = false;
     }
-    fprintf(f, R"(" style="fill:%s" />)" "\n", isContour ? "black" : "white");
+    fprintf(f, R"(" style="fill:%s" />)" "\n", insideIsBlack ? "black" : "white");
 }
 
-void writePolygonSVG(FILE * f, clp::Paths &paths, bool isContour, double scalingFactor, double minx, double miny) {
+void writePolygonSVG(FILE * f, clp::Paths &paths, bool isContour, bool insideIsBlack, double scalingFactor, double minx, double miny) {
     for (auto path = paths.begin(); path != paths.end(); ++path) {
-        writePolygonSVG(f, *path, isContour, scalingFactor, minx, miny);
+        writePolygonSVG(f, *path, isContour, insideIsBlack, scalingFactor, minx, miny);
     }
 }
 
-void writePolygonSVG(FILE * f, HoledPolygon &hp, double scalingFactor, double minx, double miny) {
-    writePolygonSVG(f, hp.contour, true, scalingFactor, minx, miny);
-    writePolygonSVG(f, hp.holes,  false, scalingFactor, minx, miny);
+void writePolygonSVG(FILE * f, HoledPolygon &hp, bool insideIsBlack, double scalingFactor, double minx, double miny) {
+    writePolygonSVG(f, hp.contour, true,   insideIsBlack, scalingFactor, minx, miny);
+    writePolygonSVG(f, hp.holes,   false, !insideIsBlack, scalingFactor, minx, miny);
 }
 
-void writePolygonSVG(FILE * f, HoledPolygons &hps, double scalingFactor, double minx, double miny) {
+void writePolygonSVG(FILE * f, HoledPolygons &hps, bool insideIsBlack, double scalingFactor, double minx, double miny) {
     for (auto hp = hps.begin(); hp != hps.end(); ++hp) {
-        writePolygonSVG(f, *hp, scalingFactor, minx, miny);
+        writePolygonSVG(f, *hp, insideIsBlack, scalingFactor, minx, miny);
     }
 }
 
-void writeSVG(const char * filename, HoledPolygons &hps, double scalingFactor, const char * units) {
+void writeSVG(const char * filename, HoledPolygons &hps, bool insideIsBlack, double scalingFactor, const char * units) {
     clp::DoublePoint size, polOffset, viewBox1, viewBox2, viewBoxSize;
     double viewBoxExtra;
     viewBoxExtra    = 0.1;
@@ -66,12 +66,15 @@ void writeSVG(const char * filename, HoledPolygons &hps, double scalingFactor, c
         R"(<svg width="%.20g%s" height="%.20g%s" viewBox="%g %g %.20g %.20g" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:slicer="http://slicer.org/namespaces/slicer">)" "\n"
         R"(  <g id="only_one_slice">)" "\n",
         viewBoxSize.X, units, viewBoxSize.Y, units, 0.0, 0.0, viewBoxSize.X, viewBoxSize.Y);
-    writePolygonSVG(f, hps, scalingFactor, polOffset.X, polOffset.Y);
+    if (!insideIsBlack) {
+        fprintf(f, R"(    <rect width="%.20g" height="%.20g" style="fill:black"/>)" "\n", viewBoxSize.X, viewBoxSize.Y);
+    }
+    writePolygonSVG(f, hps, insideIsBlack, scalingFactor, polOffset.X, polOffset.Y);
     fputs("  </g>\n</svg>", f);
     fclose(f);
 }
 
-std::string processMatches(const char * filename, const char * svgfilename, PathInFileSpec spec, bool matchFirst) {
+std::string processMatches(const char * filename, const char * svgfilename, PathInFileSpec spec, bool matchFirst, bool insideIsBlack) {
     FILE * f = fopen(filename, "rb");
     if (f == NULL) { return str("Could not open file ", filename); }
 
@@ -123,7 +126,7 @@ std::string processMatches(const char * filename, const char * svgfilename, Path
             svgname = str(svgfilename, '.', std::setfill('0'), std::setw(3), index++, ".svg");
         }
 
-        writeSVG(svgname.c_str(), hps, sliceheader.scaling, "mm");
+        writeSVG(svgname.c_str(), hps, insideIsBlack, sliceheader.scaling, "mm");
 
         if (matchFirst) {
             break;
@@ -135,9 +138,10 @@ std::string processMatches(const char * filename, const char * svgfilename, Path
 }
 
 const char *ERR =
-"\nArguments: PATHSFILENAME SVGFILENAME (first | all) [SPECTYPE VALUE]*\n\n"
+"\nArguments: PATHSFILENAME SVGFILENAME (black | white) (first | all) [SPECTYPE VALUE]*\n\n"
 "    -PATHSFILENAME is required (input paths file name).\n\n"
 "    -SVGFILENAME is required (output svg file name).\n\n"
+"    -the color inside the contours can be either 'black' or 'white'.\n\n"
 "    -if 'first' is specified, only the first eligible match is converted to SVG. If 'all' is specified, all eligible paths are converted, creating one SVG file for each one.\n\n"
 "    -Multiple pairs SPECTYPE VALUE can be specified. SPECTYPE can be either 'type', 'ntool', or 'z'. For the first, VALUE can be either r[aw], c[ontour] or t[oolpath], for the second, it is an integer, for the latter, a floating-point value. If several pairs have the same SPECTYPE, the latter overwrites the former. If nothing is specified, all paths are eligible.\n\n"
 "This tool writes as a SVG file the first record in PATHSFILENAME that matches the specification.\n\n";
@@ -156,12 +160,22 @@ int main(int argc, const char** argv) {
         return -1;
     }
 
-    const char *pathsfilename, *svgfilename, *matchMode;
-    bool matchFirst;
+    const char *pathsfilename, *svgfilename, *matchMode, *colorMode;
+    bool matchFirst, insideIsBlack;
     
     if (!rd.readParam(pathsfilename,   "PATHSFILENAME"))             { printError(rd); return -1; }
     if (!rd.readParam(svgfilename,     "SVGFILENAME"))               { printError(rd); return -1; }
+    if (!rd.readParam(colorMode,       "color (black/white)"))       { printError(rd); return -1; }
     if (!rd.readParam(matchMode,       "matching mode (first/all)")) { printError(rd); return -1; }
+
+    if (strcmp(colorMode, "black") == 0) {
+        insideIsBlack = true;
+    } else if (strcmp(colorMode, "white") == 0) {
+        insideIsBlack = false;
+    } else {
+        fprintf(stderr, "the color should be 'black' or 'white', but it is: %s\n", colorMode);
+        return -1;
+    }
 
     if (strcmp(matchMode, "first") == 0) {
         matchFirst = true;
@@ -172,7 +186,7 @@ int main(int argc, const char** argv) {
         return -1;
     }
 
-    if (!fileExists( pathsfilename)) { fprintf(stderr,  "the input file was not found: %s!!!", pathsfilename ); return -1; }
+    if (!fileExists(pathsfilename)) { fprintf(stderr, "the input file was not found: %s!!!", pathsfilename); return -1; }
 
     PathInFileSpec spec;
     std::string err = spec.readFromCommandLine(rd, -1, false);
@@ -182,7 +196,7 @@ int main(int argc, const char** argv) {
     }
 
     clp::Paths paths;
-    err = processMatches(pathsfilename, svgfilename, spec, matchFirst);
+    err = processMatches(pathsfilename, svgfilename, spec, matchFirst, insideIsBlack);
 
     if (!err.empty()) {
         fprintf(stderr, "Error while trying to get a set of paths according to the specification: %s", err.c_str());
