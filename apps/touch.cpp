d3ac2a143faa8792cb3b7078b20198aa51d8d70a
josedavid <josedavid@bestiajo.jd>
2016-02-16 17:40:21 +0100

    enable the touch command to add arbitrary values to slice headers

--- apps/touch.cpp
+++ apps/touch.cpp
@@ -8,9 +8,15 @@ typedef struct Data {
     Data(int _k, SliceHeader _header, int datasize) : k(_k), header(std::move(_header)), data(datasize) {}
 } Data;
 
-typedef std::pair<PathInFileSpec, PathInFileSpec> specpair;
+typedef std::pair<bool, T64> Add;
 
-std::string touchFile(const char * filename, const char *outputname, std::vector<specpair> &pairs) {
+typedef struct Spec {
+    PathInFileSpec filterspec;
+    PathInFileSpec setspec;
+    std::vector<Add> adds;
+} Spec;
+
+std::string touchFile(const char * filename, const char *outputname, std::vector<Spec> &specs) {
     FILE * f = fopen(filename, "rb");
     if (f == NULL) { return str("Could not open file ", filename); }
 
@@ -26,14 +32,21 @@ std::string touchFile(const char * filename, const char *outputname, std::vector
         if (!e.empty())                     { err = str("Error reading ", currentRecord, "-th slice header: ", e); break; }
         if (sliceheader.alldata.size() < 7) { err = str("Error reading ", currentRecord, "-th slice header: header is too short!"); break;  }
 
-        for (auto &pair : pairs) {
-            auto &filterspec = pair.first;
-            auto &setspec    = pair.second;
-            if (filterspec.matchesHeader(sliceheader)) {
-                if (setspec.usentool) sliceheader.ntool = setspec.ntool;
-                if (setspec.usetype)  sliceheader.type = setspec.type;
-                if (setspec.usez)     sliceheader.z = setspec.z;
+        for (auto &spec : specs) {
+            if (spec.filterspec.matchesHeader(sliceheader)) {
+                if (spec.setspec.usentool) sliceheader.ntool = spec.setspec.ntool;
+                if (spec.setspec.usetype)  sliceheader.type  = spec.setspec.type;
+                if (spec.setspec.usez)     sliceheader.z     = spec.setspec.z;
                 sliceheader.setBuffer();
+                if (!spec.adds.empty()) {
+                    auto num = spec.adds.size()*sizeof(double);
+                    sliceheader.alldata[0].i += num;
+                    sliceheader.alldata[1].i += num;
+                    sliceheader.alldata.reserve(sliceheader.alldata.size() + num);
+                    for (auto &add : spec.adds) {
+                        sliceheader.alldata.push_back(add.second);
+                    }
+                }
                 break;
             }
         }
@@ -77,14 +90,17 @@ std::string touchFile(const char * filename, const char *outputname, std::vector
 }
 
 const char *ERR =
-"\nArguments: PATHSFILENAME_INPUT PATHSFILENAME_OUTPUT [set [SPECTYPE VALUE]+ to [SPECTYPE VALUE]+]+\n\n"
+"\nArguments: PATHSFILENAME_INPUT PATHSFILENAME_OUTPUT [set [SPECTYPE VALUE]+ to [SPECTYPE VALUE]+ [add [ADDTYPE VALUE]+]]+\n\n"
 "    -PATHSFILENAME_INPUT and PATHSFILENAME_OUTPUT are required (input/output paths file names).\n\n"
-"    -a sequence of specifications 'set SPEC_MATCH to SPEC_SET', where SPEC_MATCH and SPEC_SET are lists of pairs 'SPECTYPE VALUE'.\n\n"
+"    -a sequence of specifications 'set SPEC_MATCH to SPEC_SET [add SPEC_ADD]', where SPEC_MATCH and SPEC_SET are lists of pairs 'SPECTYPE VALUE'.\n\n"
 "        -SPECTYPE can be either 'type', 'ntool', or 'z'.\n\n"
-"        -If SPECTYPE is 'type', VALUE can be either r[aw], c[ontour] or t[oolpath].\n\n"
-"        -If SPECTYPE is 'ntool', VALUE is an integer.\n\n"
-"        -If SPECTYPE is 'z', VALUE is a floating point number.\n\n"
-"This tool modifies the headers of records: if they match a SPEC_MATCH set of specifications, their values are set according to the cooresponding SPEC_SET.\n\n";
+"            -If SPECTYPE is 'type', VALUE can be either r[aw], c[ontour] or t[oolpath].\n\n"
+"            -If SPECTYPE is 'ntool', VALUE is an integer.\n\n"
+"            -If SPECTYPE is 'z', VALUE is a floating point number.\n\n"
+"        -If 'add SPEC_ADD' is specified (it is optional), a list of values are appended to the header of the matching headers. these values are specified by a list of pairs 'ADDTYPE VALUE'.\n\n"
+"            -If ADDTYPE is 'double', VALUE is a floating point number.\n\n"
+"            -If ADDTYPE is 'integer', VALUE is a 64-bit signed integer.\n\n"
+"This tool modifies the headers of records: if they match a SPEC_MATCH set of specifications, their values are set according to the cooresponding SPEC_SET and SPEC_ADD.\n\n";
 
 void printError(ParamReader &rd) {
     rd.fmt << ERR;
@@ -105,29 +121,54 @@ int main(int argc, const char** argv) {
     if (!rd.readParam(pathsfilename_input,    "PATHSFILENAME_INPUT"))           { printError(rd); return -1; }
     if (!rd.readParam(pathsfilename_output,   "PATHSFILENAME_OUTPUT"))          { printError(rd); return -1; }
 
-    std::vector<specpair> pairs;
+    std::vector<Spec> specs;
     std::string err;
 
     while (rd.argidx<rd.argc) {
         if (!rd.readKeyword("set", false, "'set' keyword")) { printError(rd); return -1; }
 
-        pairs.push_back(specpair());
-        err = pairs.back().first.readFromCommandLine(rd, -1, true);
+        specs.push_back(Spec());
+        err = specs.back().filterspec.readFromCommandLine(rd, -1, true);
         if (!err.empty()) {
-            fprintf(stderr, "Error while trying to read the %d-th filter specification: %s", pairs.size(), err.c_str());
+            fprintf(stderr, "Error while trying to read the %d-th filter specification: %s", specs.size(), err.c_str());
             return -1;
         }
 
         if (!rd.readKeyword("to", false, "'to' keyword")) { printError(rd); return -1; }
 
-        err = pairs.back().second.readFromCommandLine(rd, -1, true);
+        err = specs.back().setspec.readFromCommandLine(rd, -1, true);
         if (!err.empty()) {
-            fprintf(stderr, "Error while trying to read the %d-th set specification: %s", pairs.size(), err.c_str());
+            fprintf(stderr, "Error while trying to read the %d-th set specification: %s", specs.size(), err.c_str());
             return -1;
+        }
+
+        if (rd.argidx < rd.argc) {
+            if (rd.readKeyword("add", false, "'add' keyword")) {
+                const char *type;
+                bool isdouble;
+                if (!rd.readParam(type, "add_type (double/integer)"))           { printError(rd); return -1; }
+                if (tolower(type[0] == 'd')) {
+                    isdouble = true;
+                } else if (tolower(type[0] == 'i')) {
+                    isdouble = false;
+                } else {
+                    fprintf(stderr, "Error: add type must be either (d)ouble or (i)nteger, but it was <%s>\n", type);
+                    return -1;
+                }
+                T64 value;
+                if (isdouble) {
+                    if (!rd.readParam(value.d, "add_value (double/integer)"))           { printError(rd); return -1; }
+                } else {
+                    if (!rd.readParam(value.i, "add_value (double/integer)"))           { printError(rd); return -1; }
+                }
+                specs.back().adds.emplace_back(isdouble, value);
+            } else {
+                --rd.argidx;
+            }
         }
     }
 
-    err = touchFile(pathsfilename_input, pathsfilename_output, pairs);
+    err = touchFile(pathsfilename_input, pathsfilename_output, specs);
 
     if (!err.empty()) {
         fprintf(stderr, "Error while trying to get a set of paths according to the specification: %s", err.c_str());
