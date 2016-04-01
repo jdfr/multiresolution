3d55b39cbaeea5f80ffe4ea56c744ae4b442a0ca
josedavid <josedavid@bestiajo.jd>
2016-04-01 11:17:03 +0200

    make the windows build scripts ask for the absolute path to the python executable

--- rmk-msvs.bat
+++ rmk-msvs.bat
@@ -2,5 +2,6 @@ if exist .\CMakeLists.txt (
   echo DO NOT RUN THIS SCRIPT FROM THE BASE DIRECTORY! YOU WILL MESS EVERYTHING!!!!
   goto :eof
 )
-cmake .. -G "Visual Studio 12 2013 Win64"
+set /p PYTHON_EXECUTABLE="Enter absolute path to the Python executable (you can leave this empty if you have installed Python system-wide): "
+cmake -G "Visual Studio 12 2013 Win64" -D PYTHON_EXECUTABLE=%PYTHON_EXECUTABLE% ..
 cmake --build . --config Release
