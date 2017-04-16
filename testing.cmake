b4fb019f33b5ce2cec4cbe9a9e3bfa9878dcd66c
josedavid <josedavid@bestiajo.jd>
2017-04-16 04:51:35 +0200

    add more cmake tests, this time with non-minimal stls

--- testing.cmake
+++ testing.cmake
@@ -92,6 +92,49 @@ MACRO(TEST_MULTIRES_COMPARE)
   TEST_MULTIRES_COMPARE_CHOOSEFLAGS(--load --save ${ARGN})
 ENDMACRO()
 
+#some tests are also used to test raw save/load
+MACRO(TEST_MULTIRES_COMPARE_TESTRAW PREVTEST INPUTFILE TESTNAME THELABELS_MULTIRES THELABELS_COMP)
+  set(OARGS ${THELABELS_MULTIRES} ${THELABELS_COMP} ${ARGN})
+  #raw save test
+  TEST_MULTIRES_COMPARE_CHOOSEFLAGS(--load     --just-save-raw ${PREVTEST}         ${INPUTFILE}              ${TESTNAME}_saveraw ${OARGS})
+  #raw load test
+  TEST_MULTIRES_COMPARE_CHOOSEFLAGS(--load-raw --save          ${TESTNAME}_saveraw ${TESTNAME}_saveraw.paths ${TESTNAME}_loadraw ${OARGS})
+  #this comparison is not part of the comp* series, because that is intended to compare the results of *two* sets of results;
+  #this TEST_COMPARE is to check that the --load-raw and --just-save-raw flags work as intended 
+  TEST_COMPARE(${TESTNAME}_compareraw ${THELABELS_MULTIRES} "${TESTNAME};${TESTNAME}_loadraw" "${TEST_DIR}/${TESTNAME}.paths" "${TEST_DIR}/${TESTNAME}_loadraw.paths")
+ENDMACRO()
+
+#some tests are also used to test load-multi
+#for --load-multi, we do not do comparisons with previous sets of results
+MACRO(TEST_MULTIRES_COMPARE_TESTLOADMULTI PREVTEST1 INPUTFILE1 PREVTEST2 INPUTFILE2 TESTNAME THELABELS_MULTIRES THELABELS_COMP)
+  set(PRODUCT_ORIG "${TEST_DIR}/${TESTNAME}.paths")
+  set(PRODUCT_LD   "${TEST_DIR}/${TESTNAME}_loadmulti.paths")
+  TEST_TEMPLATE(${TESTNAME}_loadmulti "${OUTPUTDIR}" ${multires} --load-multi "${TEST_DIR}/${INPUTFILE1}" "${TEST_DIR}/${INPUTFILE2}" --save ${PRODUCT_LD} ${ARGN})
+  set_tests_properties(${TESTNAME}_loadmulti PROPERTIES LABELS "${THELABELS_MULTIRES}" DEPENDS "${PREVTEST1};${PREVTEST2}" REQUIRED_FILES "${TEST_DIR}/${INPUTFILE1};${TEST_DIR}/${INPUTFILE2}")
+  set(PRODUCTPREV "${TESTPREV_DIR}/${TESTNAME}_loadmulti.paths")
+  TEST_COMPARE(COMPARE_${TESTNAME}_loadmulti "${THELABELS_COMP}" "${TESTNAME}_loadmulti" "${PRODUCT_LD}" "${PRODUCTPREV}")
+  #no comparison with the original is done, because the results may have very small differences
+  # #this comparison is not part of the comp* series, because that is intended to compare the results of *two* sets of results;
+  # #this TEST_COMPARE is to check that the --checkpoint-save and --checkpoint-load flags work as intended 
+  # TEST_COMPARE(${TESTNAME}_compare_loadmulti ${THELABELS_MULTIRES} "${TESTNAME};${TESTNAME}_loadmulti" ${PRODUCT_ORIG} ${PRODUCT_LD})
+ENDMACRO()
+
+#some tests are also used to test checkpointing
+#for checkpointing, we do not do comparisons with previous sets of results
+MACRO(TEST_MULTIRES_TESTCHECKPOINTING PREVTEST INPUTFILE TESTNAME THELABELS_MULTIRES THELABELS_COMP)
+  #THELABELS_COMP is not used in this macro, but it is included to have the same interface as TEST_MULTIRES_COMPARE* macros, so this macro can share argument lists easily)
+  set(PRODUCT_ORIG  "${TEST_DIR}/${TESTNAME}.paths")
+  set(PRODUCT_CHKPT "${TEST_DIR}/${TESTNAME}_checkpoint.paths")
+  set(CHKPT_FILE    "${TEST_DIR}/${TESTNAME}.checkpoint")
+  #checkpoint save test
+  TEST_MULTIRES(${PRODUCT_CHKPT} --load --save ${PREVTEST}                 ${INPUTFILE} ${TESTNAME}_save_checkpoint ${THELABELS_MULTIRES} ${ARGN} --checkpoint-save ${CHKPT_FILE} 5)
+  #checkpoint load test
+  TEST_MULTIRES(${PRODUCT_CHKPT} --load --save ${TESTNAME}_save_checkpoint ${INPUTFILE} ${TESTNAME}_load_checkpoint ${THELABELS_MULTIRES} ${ARGN} --checkpoint-load ${CHKPT_FILE})
+  #this comparison is not part of the comp* series, because that is intended to compare the results of *two* sets of results;
+  #this TEST_COMPARE is to check that the --checkpoint-save and --checkpoint-load flags work as intended 
+  TEST_COMPARE(${TESTNAME}_comparecheckpoint ${THELABELS_MULTIRES} "${TESTNAME};${TESTNAME}_load_checkpoint" ${PRODUCT_ORIG} ${PRODUCT_CHKPT})
+ENDMACRO()
+
 #macro to repeat test/compare twice: once without snap, also with snap. To that end,
 #it must be supplied with a name of a macro that adds snapping to the configuration
 MACRO(TEST_MULTIRES_BOTHSNAP SNAPMACRONAME PREVTEST INPUTFILE TESTNAME)
@@ -129,10 +172,11 @@ add_custom_target(checkcompfull  COMMAND ctest --output-on-failure -L compfull)
 ###########################################################
 
 #STL files to copy
-TEST_COPY_FILE(putmini put_mini         mini.stl)
-TEST_COPY_FILE(putmini put_mini_salient mini.salient.stl)
-TEST_COPY_FILE(putfull put_full         full.stl)
-TEST_COPY_FILE(putfull put_full_dented  full.dented.stl)
+TEST_COPY_FILE(putmini put_mini             mini.stl)
+TEST_COPY_FILE(putmini put_mini_salient     mini.salient.stl)
+TEST_COPY_FILE(putfull put_full             full.stl)
+TEST_COPY_FILE(putfull put_full_justsalient salient.stl)
+TEST_COPY_FILE(putfull put_full_dented      full.dented.stl)
 #STL file to generate
 TEST_TEMPLATE(put_full_salient "${OUTPUTDIR}/${PYTHONSCRIPTS_PATH}" "${PYTHON_EXECUTABLE}" "mergestls.py" "${TEST_DIR}/full.stl" "${TEST_DIR}/salient.stl" "${TEST_DIR}/full.salient.stl")
 set_tests_properties(put_full_salient PROPERTIES
@@ -250,3 +294,125 @@ TEST_MULTIRES_BOTHSNAP(SNAPTHIN ${MINISTL} mini_3d_infilling_addsub
   ${MINI_DIMST1} ${MINI_SCHED1} --medialaxis-radius 0.5 --infill linesh 
   )
 
+
+
+###########################################################
+###### FULL-FLEDGED TEST CASES
+###########################################################
+
+set(FULLLABELS execfull compfull)
+set(FULLSTL        put_full             full.stl)
+set(FULLDENTEDSTL  put_full_dented      full.dented.stl)
+set(SALIENTSTL     put_full_justsalient salient.stl)
+set(FULLSALIENTSTL put_full_salient     full.salient.stl)
+set(NOSCHED --slicing-uniform 0.05 --save-contours --motion-planner)
+set(FULL_DIMST0 --process 0 --radx 75 --tolerances 0.75 0.01 --smoothing 0.1)
+set(FULL_DIMST1 --process 1 --radx 10 --tolerances 0.1  0.01 --smoothing 0.1)
+set(FULL_SCHED0 --voxel-profile ellipsoid --voxel-z 75 67.5)
+set(FULL_SCHED1 --voxel-profile ellipsoid --voxel-z 10 9)
+
+#TEST_MULTIRES_BOTHSNAP is not used when computing times for *_nosnap are unreasonably large or we are already testing other things such as raw save/load.
+#It is not used when it does not make sense, either (just-save-raw, DXF tests, ...).
+
+set(ARGS_FOR_TESTRAW ${FULLSTL} full_no3d_clearance_noinfilling
+  ${FULLLABELS} ${NOSCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${CLRNCE}
+  ${FULL_DIMST1} ${CLRNCE}
+  )
+TEST_MULTIRES_COMPARE(        ${ARGS_FOR_TESTRAW})
+TEST_MULTIRES_COMPARE_TESTRAW(${ARGS_FOR_TESTRAW})
+TEST_MULTIRES_COMPARE(${FULLSTL} full_no3d_clearance_infillingconcentric
+  ${FULLLABELS} ${NOSCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${CLRNCE} --infill concentric --infill-medialaxis-radius 0.5
+  ${FULL_DIMST1} ${CLRNCE} --infill concentric --infill-medialaxis-radius 0.5
+  )
+TEST_MULTIRES_BOTHSNAP(SNAPTHIN ${FULLSTL} full_no3d_clearance_infillinglines
+  ${FULLLABELS} ${NOSCHED} 
+  ${FULL_DIMST0} ${CLRNCE} --infill linesh --infill-medialaxis-radius 0.5
+  ${FULL_DIMST1} ${CLRNCE} --infill linesv --infill-medialaxis-radius 0.5
+  )
+TEST_MULTIRES_COMPARE(${FULLSTL} full_no3d_clearance_infillingrecursive
+  ${FULLLABELS} ${NOSCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${CLRNCE} --infill concentric --infill-medialaxis-radius 0.5 --infilling-recursive
+  ${FULL_DIMST1} ${CLRNCE} --infill linesh     --infill-medialaxis-radius 0.5
+  )
+TEST_MULTIRES_COMPARE(${FULLSTL} full_no3d_clearance_infillingrecursive_removeredundant
+  ${FULLLABELS} ${NOSCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${CLRNCE} --infill concentric --infill-medialaxis-radius 0.5 --infilling-recursive
+  ${FULL_DIMST1} ${CLRNCE} --infill linesh     --infill-medialaxis-radius 0.5 --radius-removecommon 0.1
+  )
+TEST_MULTIRES_COMPARE(${FULLSTL} full_no3d_addsub_medialaxis
+  ${FULLLABELS} ${NOSCHED} --addsub ${SNAPTHICK}
+  ${FULL_DIMST0} --medialaxis-radius 0.05 --infill linesh
+  ${FULL_DIMST1} --medialaxis-radius 0.5  --infill linesh
+  )
+TEST_MULTIRES_COMPARE(${FULLDENTEDSTL} full_no3d_addsub_negclosing_nooverwriting
+  ${FULLLABELS} ${NOSCHED} ${SNAPTHICK} --addsub --neg-closing 40 --overwrite-gradual 0.9 0.5 0.4 0.6 0.2 0.8 0 1
+  ${FULL_DIMST0} --infill linesh
+  ${FULL_DIMST1} --infill linesh --medialaxis-radius 0.5
+  )
+TEST_MULTIRES_COMPARE(${FULLDENTEDSTL} full_no3d_addsub_negclosing_gradual
+  ${FULLLABELS} ${NOSCHED} --addsub --neg-closing 40 --overwrite-gradual 0.9 0.5 0.4 0.6 0.2 0.8 0 1 ${SNAPTHICK}
+  ${FULL_DIMST0} --infill linesh
+  ${FULL_DIMST1} --infill linesh --medialaxis-radius 0.5
+  )
+TEST_MULTIRES_COMPARE(${FULLSTL} full_dxf_justcontours
+  ${FULLLABELS} ${NOSCHED} ${USEGRID}
+  --dxf-contours "${TEST_DIR}/full_dxf_justcontours" --dxf-format binary --dxf-by-tool #--dxf-by-z 
+  ${FULL_DIMST0} 
+  ${FULL_DIMST1} --no-toolpaths --no-preprocessing 2
+  )
+TEST_COMPARE(COMPARE_DXF_full_dxf_justcontours_N0 compfull full_dxf_justcontours
+      "${TEST_DIR}/full_dxf_justcontours.N0.dxf"
+  "${TESTPREV_DIR}/full_dxf_justcontours.N0.dxf"
+  )
+TEST_COMPARE(COMPARE_DXF_full_dxf_justcontours_N1 compfull full_dxf_justcontours
+      "${TEST_DIR}/full_dxf_justcontours.N1.dxf"
+  "${TESTPREV_DIR}/full_dxf_justcontours.N1.dxf"
+  )
+set(ARGS_FOR_TESTRAWANDCHKPT ${FULLSTL} full_3d_clearance_noinfilling
+  ${FULLLABELS} ${SCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${FULL_SCHED0} ${CLRNCE}
+  ${FULL_DIMST1} ${FULL_SCHED1} ${CLRNCE}
+  )
+TEST_MULTIRES_COMPARE(          ${ARGS_FOR_TESTRAWANDCHKPT})
+TEST_MULTIRES_COMPARE_TESTRAW(  ${ARGS_FOR_TESTRAWANDCHKPT})
+TEST_MULTIRES_TESTCHECKPOINTING(${ARGS_FOR_TESTRAWANDCHKPT})
+TEST_MULTIRES_COMPARE(${FULLSTL} full_3d_clearance_infillinglines
+  ${FULLLABELS} ${SCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${FULL_SCHED0} ${CLRNCE} --infill linesh --infill-medialaxis-radius 0.5
+  ${FULL_DIMST1} ${FULL_SCHED1} ${CLRNCE} --infill linesv --infill-medialaxis-radius 0.5
+  )
+TEST_MULTIRES_BOTHSNAP(SNAPTHICK ${FULLSTL} full_3d_clearance_infillinglines_addperimeters
+  ${FULLLABELS} ${SCHED}
+  ${FULL_DIMST0} ${FULL_SCHED0} --infill linesv --infill-medialaxis-radius 0.5 --additional-perimeters 1
+  ${FULL_DIMST1} ${FULL_SCHED1} --infill linesh --infill-medialaxis-radius 0.5 --additional-perimeters 2
+  )
+TEST_MULTIRES_BOTHSNAP(SNAPTHICK ${FULLSTL} full_3d_clearance_withsurface_infillingcrisscrosslines
+  ${FULLLABELS} ${SCHED}
+  ${FULL_DIMST0} ${FULL_SCHED0} --infill linesvh --infill-medialaxis-radius 0.5
+  ${FULL_DIMST1} ${FULL_SCHED1} --infill linesh --infill-static-mode --infill-lineoverlap -4 --surface-infill linesh --compute-surfaces-just-with-same-process false
+  )
+#because the argument list is only ALMOST compatible with --load-multi, the first arguments are different for each case
+set(ARGS_FOR_TESTLOADMULTI full_3d_clearance_vcorrection_infillinglines
+  ${FULLLABELS} ${SCHED} --vertical-correction ${SNAPTHIN}
+  ${FULL_DIMST0} ${FULL_SCHED0} ${CLRNCE} --infill linesh --infill-medialaxis-radius 0.5
+  ${FULL_DIMST1} ${FULL_SCHED1} ${CLRNCE} --infill linesh --infill-medialaxis-radius 0.5
+  )
+TEST_MULTIRES_COMPARE(              ${FULLSALIENTSTL}        ${ARGS_FOR_TESTLOADMULTI})
+TEST_MULTIRES_COMPARE_TESTLOADMULTI(${FULLSTL} ${SALIENTSTL} ${ARGS_FOR_TESTLOADMULTI})
+TEST_MULTIRES_COMPARE(${FULLSTL} full_3d_clearance_infillingrecursive
+  ${FULLLABELS} ${SCHED} ${SNAPTHIN}
+  ${FULL_DIMST0} ${FULL_SCHED0} ${CLRNCE} --infill linesh --infill-medialaxis-radius 0.5 --infilling-recursive
+  ${FULL_DIMST1} ${FULL_SCHED1} ${CLRNCE} --infill linesh --infill-medialaxis-radius 0.5
+  )
+TEST_MULTIRES_COMPARE(${FULLSTL} full_3d_addsub_infillinglines
+  ${FULLLABELS} ${SCHED} --addsub ${SNAPTHICK}
+  ${FULL_DIMST0} ${FULL_SCHED0} --medialaxis-radius 0.1 0.05 --infill linesh
+  ${FULL_DIMST1} ${FULL_SCHED1} --medialaxis-radius 0.5      --infill linesh
+  )
+TEST_MULTIRES_BOTHSNAP(SNAPTHICK ${FULLSTL} full_3d_addsub
+  ${FULLLABELS} ${SCHED} --addsub
+  ${FULL_DIMST0} ${FULL_SCHED0} --medialaxis-radius 0.1 0.05
+  ${FULL_DIMST1} ${FULL_SCHED1} --medialaxis-radius 0.5
+  )
